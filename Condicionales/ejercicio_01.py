f1638bb991fddf46e12276354f673c78bba39802
Juan Jose Dominguez Hernandez <jujodomhdez@gmail.com>
2026-02-19 09:32:16 -0600

    Nuevoo Commit

---
+++ Condicionales/ejercicio_01.py
@@ -0,0 +1,9 @@
+#Algoritmo que pida dos números e indique si el primero es mayor que el segundo.
+
+num1 = int(input("Dime el numero 1: "))
+num2 = int(input("Dime el numero 2: "))
+
+if num1 > num2:
+	print(num1, "es mayor que", num2)
+else:
+	print(num1, "NO es mayor que", num2)
